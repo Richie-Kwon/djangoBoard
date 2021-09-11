d6b5000fb30c75aa2ed333d9bc74b6d31c886853
richie <richie.kwon1@gmail.com>
2021-09-11 14:18:41 +0100

    creating user model

--- community/user/models.py
+++ community/user/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(
+        max_length=64,
+        verbose_name='username'
+    )
+
+    password = models.CharField(
+        max_length=64,
+        verbose_name='password'
+    )
+
+    registered_dttm = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='registered_date'
+    )
+
+    class Meta:
+        db_table ='user'
+
+
+       
